168fa0778bf2dfd5e561cba2fc449022ba420726
Huy Trinh <trinhphandinhhuy@gmail.com>
2016-06-27 20:04:42 +0300

    First commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict'
+var request = require('supertest'),
+    app = require('../app').getApp;
+    
+describe('GET /', function () {
+   it('expect HTTP response 200', function (done) {
+     request(app)
+     .get('/')
+     .expect(200, done);
+   });
+});
